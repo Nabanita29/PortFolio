9c9cb3f8db9c05debdb7683b47140510267e42c7
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2023-12-26 09:40:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  menu.classList.toggle("open");
+}
+
+// an inbuilt function that is called when the page is loaded
+// meaning of the javascript code below is that when the page is loaded
+// the toggleMenu function is called
